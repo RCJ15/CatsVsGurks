caa75aeb345c1eaaa97b68444e34a0fcdb28dbea
RCJ15 <73841786+RCJ15@users.noreply.github.com>
2026-03-17 23:45:29 +0100

    CatBase fix?

--- Assets/Scripts/Towers/PlayerBase.cs
+++ Assets/Scripts/Towers/PlayerBase.cs
@@ -9,5 +9,27 @@ public class PlayerBase : Tower
         base.Awake();
 
         Instance = this;
+
+        Player.MaxHP = MaxHP;
+        Player.HP = HP;
+    }
+
+    public override void Hurt(float damage, Entity from)
+    {
+        base.Hurt(damage, from);
+
+        Player.MaxHP = MaxHP;
+        Player.HP = HP;
+    }
+
+    public override void Die()
+    {
+        base.Die();
+
+        Player.MaxHP = MaxHP;
+        Player.HP = 0;
+
+        // Death
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
     }
 }
